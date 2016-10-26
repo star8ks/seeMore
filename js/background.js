750b9d292ba42f427ad7531471e560afc73de36f
ray7551 <ray7551@gmail.com>
2016-10-27 01:43:46 +0800

    更改封装方式

--- js/background.js
+++ js/background.js
@@ -1,98 +1,68 @@
-
-
-var listener = {
-  onTabCreated: function (tabInfo) {
-    // clog(tabInfo);
-    // Since chrome.tabs.onCreated listener may not get tab.url properly,
-    // but It SEEMS that chrome.tabs.get listener will be called
-    // when tab.url is ready.
-    // This way takes shorter time compare to chrome.tabs.onUpdated
-    chrome.tabs.get(tabInfo.id, function (tab) {
-      // clog('tabs.onCreated + tabs.get----------->tab.url', tab.url);
-      if (tab.url && localStorage['remove_redirect'] && Url.isGoogleRedirect(tab.url)) {
-        listener.removeRedirect(tab);
-      }
-    });
-  },
-
-  onTabUpdated: function (tabId, changeInfo, tab) {
-    if (!changeInfo.status || 'loading' != changeInfo.status) {
-      return;
-    }
-    // clog('changeInfo.url', changeInfo.url);
-    // clog('changeInfo.status', changeInfo.status);
-    // clog.info('tab updated! tab.url', tab.url);
-
-    if (/^chrome/.test(tab.url)) {
-      localStorage["word"] = '';
-      return;
-    }
-
-    if(tab.url){
-      if (tab.url && localStorage['remove_redirect'] && Url.isGoogleRedirect(tab.url)) {
-        listener.removeRedirect(tab);
-      }
-    }
-
-    // TODO: inRequiredHost or inCustomHost
-    if (Url.inRequiredHost(tab.url)) {
-      chrome.browserAction.setTitle({title: '点击切换搜索引擎', tabId: tabId});
-    }
-  },
-
-  removeRedirect: function (tab) {
+var Listener = (function () {
+  function removeRedirect(tab) {
     var originUrl = Url.getQueryVal(tab.url, 'url');
     var originImgUrl = Url.getQueryVal(tab.url, 'imgrefurl');
     if (originUrl) {
-      clog.info('█████Remove redirection: ', tab.url, ' to ', decodeURIComponent(originUrl));
+      // clog.info('█████Remove redirection: ', tab.url, ' to ', decodeURIComponent(originUrl));
       chrome.tabs.update(tab.id, {url: decodeURIComponent(originUrl)});
     } else if (originImgUrl) {
-      clog.info('█████Remove redirection: ', tab.url, ' to ', decodeURIComponent(originImgUrl));
+      // clog.info('█████Remove redirection: ', tab.url, ' to ', decodeURIComponent(originImgUrl));
       chrome.tabs.update(tab.id, {url: decodeURIComponent(originImgUrl)});
     }
-  },
+  }
 
-  onInstalled: function() {
-    var manifest = chrome.runtime.getManifest();
-    CONFIG.defaultEngine.forEach(function (searchEngine) {
-      var index = CONFIG.engineClassPre + searchEngine;
-      localStorage[index] = 'checked';
-    });
-    localStorage['remove_redirect'] = 'checked';
-    localStorage['version'] = manifest.version;
+  return {
+    onTabCreated: function (tabInfo) {
+      // Since chrome.tabs.onCreated listener may not get tab.url properly,
+      // but It SEEMS that chrome.tabs.get listener will be called
+      // AFTER tab.url is ready.
+      // This way takes shorter time compare to chrome.tabs.onUpdated
+      chrome.tabs.get(tabInfo.id, function (tab) {
+        // clog('tabs.onCreated + tabs.get----------->tab.url', tab.url);
+        if (localStorage['remove_redirect'] && Url.isGoogleRedirect(tab.url)) {
+          removeRedirect(tab);
+        }
+      });
+    },
 
-    if (!CONFIG.devMode) {
-      chrome.tabs.create({url: 'chrome://extensions/?options=' + chrome.runtime.id});
+    onTabUpdated: function (tabId, changeInfo, tab) {
+      if (!changeInfo.status || 'loading' != changeInfo.status || !tab.url) {
+        return;
+      }
+      if (/^chrome/.test(tab.url)) {
+        localStorage["word"] = '';
+        return;
+      }
+
+      if (localStorage['remove_redirect'] && Url.isGoogleRedirect(tab.url)) {
+        removeRedirect(tab);
+      }
+
+      // TODO: inRequiredHost or inCustomHost
+      if (Url.inRequiredHost(tab.url)) {
+        chrome.browserAction.setTitle({title: '点击切换搜索引擎', tabId: tabId});
+      }
+    },
+
+    onInstalled: function () {
+      var manifest = chrome.runtime.getManifest();
+      CONFIG.defaultEngine.forEach(function (searchEngine) {
+        var index = CONFIG.engineClassPre + searchEngine;
+        localStorage[index] = 'checked';
+      });
+      localStorage['remove_redirect'] = 'checked';
+      localStorage['version'] = manifest.version;
+
+      if (!CONFIG.devMode) {
+        chrome.tabs.create({url: 'chrome://extensions/?options=' + chrome.runtime.id});
+      }
     }
   }
-};
+})();
 
-chrome.runtime.onInstalled.addListener(listener.onInstalled);
-chrome.tabs.onCreated.addListener(listener.onTabCreated);
-
+chrome.runtime.onInstalled.addListener(Listener.onInstalled);
+chrome.tabs.onCreated.addListener(Listener.onTabCreated);
 // Listen for any changes to the URL of any tab.
-chrome.tabs.onUpdated.addListener(listener.onTabUpdated);
-
-
-// 从 url 提取目的页地址直接访问
-// chrome.browserAction.onClicked.addListener(function actionClick(tab) {
-//   if (Url.getQueryVal(tab.url, 'url')) {
-//     chrome.tabs.update(tab.id, {url: decodeURIComponent(args['url'])});
-//   } else if (Url.getQueryVal(tab.url, 'imgrefurl')){
-//     chrome.tabs.update(tab.id, {url: decodeURIComponent(args['imgrefurl'])});
-//   }
-// });
-
-// Listen for the content script to send a message
-// chrome.runtime.onMessage.addListener(function onRequest(request) {
-//   if (request.search) {
-//     localStorage["word"] = request.search;
-//   }
-//   if (request.browserEvent === 'urlChanged') {
-//     clog('updated from contentscript');
-//   }
-// });
-
+chrome.tabs.onUpdated.addListener(Listener.onTabUpdated);
 
 // @TODO 兼容旧版本数据
-
