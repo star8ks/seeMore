6999514d31d86294039e92acb3b94cccffc88af0
ray7551 <ray7551@gmail.com>
2016-12-22 01:45:58 +0800

    add eachOther param

--- src/common/base/includeString.js
+++ src/common/base/includeString.js
@@ -7,9 +7,10 @@
  * @param {Number} [start=0]
  * @param {Boolean} [caseSensitive=false]
  * */
-String.prototype.includeString = function (search, start, caseSensitive) {
+String.prototype.includeString = function (search, start, caseSensitive, eachOther) {
   'use strict';
   caseSensitive = caseSensitive === void 0 ? false : !!caseSensitive;
+  eachOther = eachOther === void 0 ? false : !!eachOther;
   if (caseSensitive) {
     return this.includes(search, start);
   }
@@ -18,7 +19,8 @@ String.prototype.includeString = function (search, start, caseSensitive) {
   }
   if (start + search.length > this.length) return false;
 
-  return this.toLowerCase().indexOf(search.toLowerCase(), start) !== -1;
+  var include = this.toLowerCase().indexOf(search.toLowerCase(), start) !== -1;
+  return !eachOther ? include : (include || search.toLowerCase().indexOf(this.toLowerCase()) !== -1);
 };
 
 export default String.prototype.includeString;
