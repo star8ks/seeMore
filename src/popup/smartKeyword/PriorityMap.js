7a4654b144a972ecf6900167482d336c3c4ee1ff
ray7551 <ray7551@gmail.com>
2016-12-23 16:50:19 +0800

    case insensitive map key

--- src/popup/smartKeyword/PriorityMap.js
+++ src/popup/smartKeyword/PriorityMap.js
@@ -1,5 +1,42 @@
 import {PRINTABLE_ASCII, KEYWORD_BLACKLIST} from './const';
 /**
+ * Just like normal Map, but use case insensitive string as key
+ * */
+class StringMap{
+  constructor() {
+    this.map = {};
+  }
+  get size() {
+    return Object.keys(this.map).length;
+  }
+  has(key) {
+    if(typeof key !== 'string') throw new Error('key must be a string, but given:' + key);
+    return Object.keys(this.map).includes(key.toLowerCase());
+  }
+  set(key, val) {
+    if(typeof key !== 'string') throw new Error('key must be a string, but given:' + key);
+    if(val === undefined) throw new Error('value cannot be undefined');
+    this.map[key.toLowerCase()] = Object.defineProperties({}, {
+      'val': {value: val, enumerable: true},
+      '$originKey': {value: key}
+    });
+  }
+  get(key) {
+    if(typeof key !== 'string') throw new Error('key must be a string, but given:' + key);
+    let valObj = this.map[key.toLowerCase()];
+    return valObj===undefined ? undefined : valObj.val;
+  }
+  clear() {
+    this.map = {};
+  }
+  *[Symbol.iterator]() {
+    let keys = Object.keys(this.map);
+    for(let key of keys) {
+      yield [this.map[key].$originKey, this.map[key].val];
+    }
+   }
+}
+/**
  * @author ray7551@gmail.com
  */
 class PriorityMap {
@@ -11,10 +48,10 @@ class PriorityMap {
     Object.keys(PriorityMap.confidence).forEach(function (key) {
       PriorityMap.confidence[key] = baseConfidence * PriorityMap.confidence[key];
     });
-    this.map = new Map();
-    this.siteKeywords = new Map();
+    this.map = new StringMap();
+    this.siteKeywords = new StringMap();
     this._setSiteKeywords(url);
-    this.vipWords = new Map();
+    this.vipWords = new StringMap();
   }
 
   /**
@@ -22,9 +59,11 @@ class PriorityMap {
    * @return {Boolean}
    * */
   static _inBlackList(word) {
+    word = word.toLowerCase();
     return KEYWORD_BLACKLIST.includes(word)
       || /^\d+$/.test(word)
       || word.length <= 0
+      || (word.length <= 2 && word === word.toLowerCase())
       || (word.length ===1 && new RegExp(`[${PRINTABLE_ASCII}À-ÿ]`).test(word));
   }
 
@@ -34,7 +73,7 @@ class PriorityMap {
    * */
   static _preProcess(word) {
     // @TODO remove head and tail punctuations(common ending punct) here? consider: ect... .Net <header>
-    return word.toLowerCase().trim();
+    return word.trim();
   }
 
   /**
@@ -48,12 +87,11 @@ class PriorityMap {
         return;
       }
       this.siteKeywords.set(word, PriorityMap.confidence.site);
-    })
+    });
   }
 
   addVipWords(word, confidence = PriorityMap.confidence.vip) {
     word = PriorityMap._preProcess(word);
-    console.log(this.siteKeywords.has(word));
     if (PriorityMap._inBlackList(word) || this.siteKeywords.has(word)) return;
 
     this.vipWords.set(
@@ -93,6 +131,7 @@ class PriorityMap {
       : this.map.set(key, this.vipWords.has(key) ? this.vipWords.get(key) + increment : increment);
   }
 }
-PriorityMap.confidence = {base: 1, site: .1, vip: .5};
+PriorityMap.confidence = {base: 1, site: .1, vip: .49};
 
 export default PriorityMap;
+export {StringMap, PriorityMap};
--- test/popup/smarkKeyword/PriorityMap.spec.js
+++ test/popup/smarkKeyword/PriorityMap.spec.js
@@ -1,11 +1,101 @@
-const chai  = require('chai');
+const chai = require('chai');
 const dirtyChai = require('dirty-chai');
 import Url from '../../../src/common/Url';
-import PriorityMap from '../../../src/popup/smartKeyword/PriorityMap';
+import {StringMap, PriorityMap} from '../../../src/popup/smartKeyword/PriorityMap';
 
 chai.config.includeStack = true;
 chai.use(dirtyChai);
 var should = chai.should();
+
+describe('StringMap', () => {
+  describe('set', () => {
+    it('should set values in map', () => {
+      let m = new StringMap();
+      m.set('s', 4);
+      m.map.should.eql({s: {val: 4}});
+    });
+    it('should be case insensitive', () => {
+      let m = new StringMap();
+      m.set('s', 4);
+      m.set('S', 5);
+      m.map.should.eql({s: {val: 5}});
+    });
+    it('should save the origin key as it last seted', () => {
+      let m = new StringMap();
+      m.set('sE', 4);
+      m.set('Se', 5);
+      m.map.should.eql({se: {val: 5}});
+      m.map.se.$originKey.should.equal('Se');
+    });
+    it('should only accept string as key', () => {
+      let m = new StringMap();
+      (function(){
+        m.set(4)
+      }).should.Throw(Error, /key must be a string/);
+    });
+    it('should throw Error if val undefined', () => {
+      let m = new StringMap();
+      (() => m.set('3')).should.Throw(Error, /value cannot be undefined/);
+    });
+  });
+  describe('has', () => {
+    let m = new StringMap();
+    m.set('t', 3);
+    it('should be case insensitive', () => {
+      m.has('t').should.be.true();
+      m.has('T').should.be.true();
+    });
+    it('should only accept string as argument', () => {
+      (() => m.has(4)).should.Throw(Error, /key must be a string/);
+    });
+  });
+  describe('get', () => {
+    let m = new StringMap();
+    m.set('t', 5);
+    it('should return undefined if not exist', () => {
+      (m.get('e') === undefined).should.be.true();
+    });
+    it('should be case insensitive', () => {
+      m.get('T').should.equal(5);
+    });
+    it('should only accept string as argument', () => {
+      (() => m.get(4)).should.Throw(Error, /key must be a string/);
+    });
+  });
+  describe('clear', () => {
+    it('should clear all', () => {
+      let m = new StringMap();
+      m.set('t', 4);
+      m.clear();
+      m.map.should.eql({});
+    });
+  });
+  describe('iterability', () => {
+    // it('should')
+    it('should iterable', () => {
+      let m = new StringMap();
+      m.set('1', 1);
+      m.set('2', 2);
+      m.set('3', 3);
+      let values = [];
+      for(let v of m) {
+        values.push(v);
+      }
+      values.should.eql([['1', 1], ['2', 2], ['3', 3]]);
+    });
+    it('return key should be originKey', () => {
+      let m = new StringMap();
+      m.set('t', 1);
+      m.set('T', 2);
+      m.set('TEST', 3);
+      let values = [];
+      for(let v of m) {
+        values.push(v);
+      }
+      values.should.eql([['T', 2], ['TEST', 3]]);
+    });
+  });
+});
 
 describe('PriorityMap', () => {
   describe('construct', () => {
@@ -13,7 +103,7 @@ describe('PriorityMap', () => {
       it('should initialize .map', () => {
         let url = new Url('https://google.com/search?q=regex+english&safe=off#newwindow=1&safe=off&q=regex');
         let pm = new PriorityMap(url);
-        pm.map.should.be.a('Map');
+        pm.map.should.be.instanceof(StringMap);
         [...pm.map].should.be.eql([]);
         [...pm.siteKeywords].should.be.eql([['google', PriorityMap.confidence.site]]);
         [...pm.vipWords].should.be.eql([]);
@@ -26,10 +116,12 @@ describe('PriorityMap', () => {
         [...pm.siteKeywords].map((item) => item[0]).should.not.include('com');
       });
       it('should not add blacklist words', () => {
-        let url = new Url('https://www.not.google.com');
+        let url = new Url('https://www.not.vs.google.com');
         let pm = new PriorityMap(url);
-        [...pm.siteKeywords].map((item) => item[0]).should.not.include('www');
-        [...pm.siteKeywords].map((item) => item[0]).should.not.include('not');
+        let addedWords = [...pm.siteKeywords].map((item) => item[0]);
+        addedWords.should.not.include('www');
+        addedWords.should.not.include('not');
+        addedWords.should.not.include('vs');
       });
       it('should be case-insensitive', () => {
         let url = new Url('https://www.Google.google.com');
@@ -48,21 +140,130 @@ describe('PriorityMap', () => {
       pm.addVipWords('test');
       [...pm.vipWords].should.eql([['test', PriorityMap.confidence.vip]]);
     });
+    it('should accept customized priority', () => {
+      pm.addVipWords('test', 4 * PriorityMap.confidence.base);
+      [...pm.vipWords].should.eql([['test', 4 * PriorityMap.confidence.base]]);
+    });
     it('should be case-insensitive', () => {
-      pm.addVipWords('test');
       pm.addVipWords('Test');
+      pm.addVipWords('test');
       [...pm.vipWords].map(group => group[0]).should.eql(['test']);
     });
     it('should ignore blacklist words and siteKeywords', () => {
       pm.addVipWords('google');
       pm.addVipWords('Google');
       pm.addVipWords('not');
+      pm.addVipWords('vs');
       [...pm.vipWords].should.eql([]);
     });
     it('should increase confidence if word already exist', () => {
-      pm.addVipWords('test');
       pm.addVipWords('Test');
+      pm.addVipWords('test');
       [...pm.vipWords].should.eql([['test', PriorityMap.confidence.vip * 2]]);
     });
   });
+  describe('vipArray', () => {
+    let url = new Url('https://www.google.com');
+    let pm = new PriorityMap(url);
+    afterEach(() => {
+      pm.vipWords.clear();
+    });
+    it('should return an empty array if no added words', () => {
+      pm.vipArray.should.eql([]);
+    });
+    it('should be ordered array of vipWords', () => {
+      pm.addVipWords('test');
+      pm.addVipWords('test');
+      pm.addVipWords('test');
+      pm.addVipWords('test1');
+      pm.addVipWords('test1');
+      pm.addVipWords('test2');
+      pm.vipArray.should.eql([
+        {word: 'test', confidence: 3 * PriorityMap.confidence.vip},
+        {word: 'test1', confidence: 2 * PriorityMap.confidence.vip},
+        {word: 'test2', confidence: PriorityMap.confidence.vip}
+      ]);
+    });
+  });
+  describe('increaseConfidence', () => {
+    let url = new Url('https://www.google.com');
+    let pm = new PriorityMap(url);
+    afterEach(() => {
+      pm.map.clear();
+      pm.vipWords.clear();
+    });
+    it('should added to map', () => {
+      pm.increaseConfidence('test');
+      [...pm.map].should.eql([['test', PriorityMap.confidence.base]]);
+    });
+    it('should accept customized priority', () => {
+      pm.increaseConfidence('test', 4 * PriorityMap.confidence.base);
+      [...pm.map].should.eql([['test', 4 * PriorityMap.confidence.base]]);
+    });
+    it('should be case-insensitive', () => {
+      pm.increaseConfidence('Test');
+      pm.increaseConfidence('test');
+      [...pm.map].map(group => group[0]).should.eql(['test']);
+    });
+    it('should ignore blacklist words', () => {
+      pm.increaseConfidence('not');
+      [...pm.map].should.eql([]);
+    });
+    it('should use siteWord\'s priority to increase if adding word exist in siteWords', () => {
+      let originPriority = PriorityMap.confidence.base * 8;
+      pm.map.set('google', originPriority);
+      pm.increaseConfidence('google');
+      [...pm.map].should.eql([['google', originPriority + pm.siteKeywords.get('google')]]);
+    });
+    it('should based on vipWord\'s priority to add if adding NEW word exist in vipWords', () => {
+      pm.addVipWords('test', 5);
+      pm.increaseConfidence('test');
+      [...pm.map].should.eql([['test', 5 + PriorityMap.confidence.base]]);
+    });
+    it('should increase priority if adding word already exist in map', () => {
+      pm.increaseConfidence('test');
+      pm.increaseConfidence('test');
+      [...pm.map].should.eql([['test', PriorityMap.confidence.base * 2]]);
+    });
+    it('should save word as it lastest seted', () => {
+      pm.increaseConfidence('TEST');
+      pm.increaseConfidence('test');
+      pm.increaseConfidence('tESt');
+      [...pm.map].map(group => group[0]).should.eql(['tESt']);
+    });
+  });
+  describe('orderedArray', () => {
+    let url = new Url('https://www.google.com');
+    let pm = new PriorityMap(url);
+    afterEach(() => {
+      pm.map.clear();
+      pm.vipWords.clear();
+    });
+    it('should return an empty array if no added words', () => {
+        pm.orderedArray.should.eql([]);
+    });
+    it('should be ordered array of added words', () => {
+      pm.increaseConfidence('test');
+      pm.increaseConfidence('test');
+      pm.increaseConfidence('test');
+      pm.increaseConfidence('test1');
+      pm.increaseConfidence('test1');
+      pm.increaseConfidence('test2');
+      pm.orderedArray.should.eql([
+        {word: 'test', confidence: 3 * PriorityMap.confidence.base},
+        {word: 'test1', confidence: 2 * PriorityMap.confidence.base},
+        {word: 'test2', confidence: PriorityMap.confidence.base}
+      ]);
+    });
+  });
+  describe('clear', () => {
+    it('should clear map', () => {
+      let url = new Url('https://www.google.com');
+      let pm = new PriorityMap(url);
+      pm.increaseConfidence('test');
+      pm.clear();
+      pm.map.size.should.equal(0);
+    });
+  });
+
 });
