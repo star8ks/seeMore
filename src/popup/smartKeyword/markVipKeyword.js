5b016ee28c4f2e1fe11abdde9d1505f4643a5c2c
ray7551 <ray7551@gmail.com>
2016-12-24 02:25:09 +0800

    use util.regex to replace String.raw

--- src/popup/smartKeyword/markVipKeyword.js
+++ src/popup/smartKeyword/markVipKeyword.js
@@ -8,8 +8,8 @@
 import {regex} from '../../common/base';
 import {CJK, CJK_PUNCT, PUNCT, KEYWORD_BLACKLIST} from './const';
 // ASCII characters those not break word meaning
-const ASCII_CHAR = String.raw`a-zA-Z\d~&*+'\-`;
-const ASCII_PUNCT = String.raw`/_,\.<>\?\`!@#\$%\^=";\:\[\]{}\|\(\)\\`;
+const ASCII_CHAR = regex`a-zA-Z\d~&*+'\-`;
+const ASCII_PUNCT = regex`/_,\.<>\?\`!@#\$%\^=";\:\[\]{}\|\(\)\\`;
 const lGuimets = PUNCT.guillemets.left.reduce((all, current) => {
   all += current;
   return all;
@@ -20,42 +20,16 @@ const rGuimets = PUNCT.guillemets.right.reduce((all, current) => {
 }, '');
 
 let markUpperWord = function (str) {
-  // let upperSubstrRegex = new RegExp(String.raw`(^|[^${CJK}a-zA-Z${lGuimets}]|[${CJK_PUNCT}。.\s])([A-Z]{2,}[${ASCII_CHAR}]*(?:\s+[A-Z]{2,}[${ASCII_CHAR}]*)*)([^${CJK}A-Z${rGuimets}]|[${CJK_PUNCT}.\s]|$)`, 'g'); // ?《YEAH》。MAGIC 《LEAP》    ?《YEAH》.MAGIC 《LEAP》
-  // let upperSubstrRegex = new RegExp(String.raw`(^|[^${CJK}a-zA-Z${lGuimets}]|\b)([A-Z]{2,}[${ASCII_CHAR}]*(?:\s+[A-Z]{2,}[${ASCII_CHAR}]*)*)([^${CJK}A-Z${rGuimets}]|\b|$)`, 'g'); // 的《MAX》是    Is 《《MAGIC》》 leap
-  // (?<![a-zA-Z‹«《〈『])\b([A-Z]{2,}[a-zA-Z\d~@&*()_+'\-]*(?:\s+[A-Z]{2,}[a-zA-Z\d~@&*()_+'\-]*)*)\b([^A-Z›»》〉』。]|[。]|$)
-  // console.log(upperSubstrRegex)
-  // let upperSubstrRegex = new RegExp(String.raw`(^|[^${CJK}a-zA-Z${lGuimets}]|\b)\b([A-Z]{2,}[${ASCII_CHAR}]*(?:\s+[A-Z]{2,}[${ASCII_CHAR}]*)*)\b([^${CJK}A-Z${rGuimets}]|$)`, 'g'); // should ignore upper word surrounded by CJK characters
-  // return str.replace(upperSubstrRegex, '$1《$2》$3');
-  // let upperSubstrRegex = new RegExp(String.raw`(^|[^${CJK}a-zA-Z${lGuimets}])([A-Z]{2,}[${ASCII_CHAR}]*(?:\s+[A-Z]{2,}[${ASCII_CHAR}]*)*)\b`, 'g');
-  // return str.replace(upperSubstrRegex, '$1《$2》');
-
-  // let upperSubstrRegex = new RegExp(String.raw`(^|[^${CJK}a-zA-Z${lGuimets}])(\s*)([A-Z]{2,}[${ASCII_CHAR}]*(?:\s+[A-Z]{2,}[${ASCII_CHAR}]*)*)\b(.*)`, 'g');
-  // return str.replace(upperSubstrRegex, (...args) => {
-  //   let p1 = args[1], p2 = args[2] || '', p3 = args[3], p4 = args[4] || '', offset = args[5], string = args[6];
-  //   if(p4 && new RegExp(`[${CJK}]`).test(p4)) {
-  //     console.log(string);
-  //     return `${p1}${p2}${p3}${p4}`;
-  //   }
-  //   // avoid 服TAXI MAN => 服TAXI 《MAN》
-  //   if (/\s/.test(p1)) {
-  //     return `${p1}${p2}${p3}${p4}`;
-  //   }
-  //   return `${p1}${p2}《${p3}》${p4}`;
-  // });
-
-  let upperSubstrRegex = new RegExp(String.raw`(^|[^${CJK}a-zA-Z${lGuimets}])([A-Z]{2,}[${ASCII_CHAR}]*(?:\s+[A-Z]{2,}[${ASCII_CHAR}]*)*)\b(?![${CJK}])`, 'g');// ((\s+(?![A-Z]))?)
+  let upperSubstrRegex = new RegExp(regex`(^|[^${CJK}a-zA-Z${lGuimets}])([A-Z]{2,}[${ASCII_CHAR}]*(?:\s+[A-Z]{2,}[${ASCII_CHAR}]*)*)\b(?![${CJK}])`, 'g');// ((\s+(?![A-Z]))?)
   return str.replace(upperSubstrRegex, '$1《$2》');
-  // return str.replace(upperSubstrRegex, (match, p1, p2, p3='') => {
-  //   return `${p1}《${p2}》${p3}`;
-  // });
 };
 
 let markEnWord = function (str) {
-  // let vipSubstrRegex = new RegExp(String.raw`(^|[${CJK}${CJK_PUNCT}${rGuimets}]|\s)([a-z]{2,}[${ASCII_CHAR}]*(?:\s+[${ASCII_CHAR}]+)*)([${CJK}${CJK_PUNCT}${lGuimets}]|\s)`, 'ig');
-  let vipSubstrRegex0 = new RegExp(String.raw`(^)(\s*)([a-z]{2,}[${ASCII_CHAR}]*(?:\s+[${ASCII_CHAR}]+)*)(_*)\b(\s*|[${ASCII_PUNCT}]*)([${CJK}])`, 'ig');
-  let vipSubstrRegex1 = new RegExp(String.raw`([${CJK}])(\s*)([a-z]{2,}[${ASCII_CHAR}]*(?:\s+[${ASCII_CHAR}]+)*)\b`, 'ig');
-  let vipSubstrRegex2 = new RegExp(String.raw`([${rGuimets}${CJK_PUNCT}])(\s*)([a-z]{2,}[${ASCII_CHAR}]*(?:\s+[${ASCII_CHAR}]+)*)\b([^${ASCII_PUNCT}\s])`, 'ig');
-  let vipSubstrRegex3 = new RegExp(String.raw`([${ASCII_PUNCT}])(\s*)([a-z]{2,}[${ASCII_CHAR}]*(?:\s+[${ASCII_CHAR}]+)*)(_*)\b(\s*|[${ASCII_PUNCT}]*)([${CJK}])`, 'ig');
+  // let vipSubstrRegex = new RegExp(regex`(^|[${CJK}${CJK_PUNCT}${rGuimets}]|\s)([a-z]{2,}[${ASCII_CHAR}]*(?:\s+[${ASCII_CHAR}]+)*)([${CJK}${CJK_PUNCT}${lGuimets}]|\s)`, 'ig');
+  let vipSubstrRegex0 = new RegExp(regex`(^)(\s*)([a-z]{2,}[${ASCII_CHAR}]*(?:\s+[${ASCII_CHAR}]+)*)(_*)\b(\s*|[${ASCII_PUNCT}]*)([${CJK}])`, 'ig');
+  let vipSubstrRegex1 = new RegExp(regex`([${CJK}])(\s*)([a-z]{2,}[${ASCII_CHAR}]*(?:\s+[${ASCII_CHAR}]+)*)\b`, 'ig');
+  let vipSubstrRegex2 = new RegExp(regex`([${rGuimets}${CJK_PUNCT}])(\s*)([a-z]{2,}[${ASCII_CHAR}]*(?:\s+[${ASCII_CHAR}]+)*)\b([^${ASCII_PUNCT}\s])`, 'ig');
+  let vipSubstrRegex3 = new RegExp(regex`([${ASCII_PUNCT}])(\s*)([a-z]{2,}[${ASCII_CHAR}]*(?:\s+[${ASCII_CHAR}]+)*)(_*)\b(\s*|[${ASCII_PUNCT}]*)([${CJK}])`, 'ig');
   let modify = function(match, p1, p2, p3, p4) {
     return p2 ? `${p1}${p2}《${p3}》${p4}` : `${p1}《${p3}》${p4}`;
   };
@@ -69,35 +43,37 @@ let markEnWord = function (str) {
 };
 
 let markEnds = function (str) {
-  let beginRegex = new RegExp(String.raw`^([${ASCII_CHAR}]+)》`);
-  let tailRegex = new RegExp(String.raw`《([${ASCII_CHAR}]+)$`);
+  let beginRegex = new RegExp(regex`^([${ASCII_CHAR}]+)》`);
+  let tailRegex = new RegExp(regex`《([${ASCII_CHAR}]+)$`);
   return str.replace(beginRegex, '《$1》').replace(tailRegex, '《$1》');
 };
 
 let markName = function (str) {
-  let validCharacter = String.raw`a-zÀ-ÿ`;
-  let name = String.raw`[A-Z][${validCharacter}]+`;
+  let validCharacter = regex`a-zÀ-ÿ`;
+  let name = regex`[A-Z][${validCharacter}]+`;
   let subElement = regex`(?:[Nn]o.\s?\d+)|(?:${name})`;
-  let nameRegex1 = new RegExp(String.raw`(^|[^${CJK}a-zA-Z${lGuimets}])((?:${subElement})(?:\s+(?:${subElement}))+)\b(?![${CJK}])`, 'g');// ((\s+(?![A-Z]))?)
+  let nameRegex1 = new RegExp(regex`
+    (^|[^${CJK}a-zA-Z${lGuimets}])
+    ((?:${subElement})(?:\s+(?:${subElement}))+)
+    \b
+    (?![${CJK}])
+  `, 'g');// ((\s+(?![A-Z]))?)
   // /[A-Z][a-z]+\s*[-_/]\s*[A-Z][a-z]+/.test('javascript - Mocha / Chai expect.se')
-  let nameRegex2 = new RegExp(regex`(^|[^${CJK}a-zA-Z${lGuimets}])((?:${subElement})(?:\s*[-_/]\s*(?:${subElement}))+)(?![${CJK}])`, 'g');
-  // let nameRegex2 = new RegExp(regex`
-  // (^|[-_/]|\s)
-  // ( # p2: the 《name》
-  //   (?:${subElement})
-  //   (?:
-  //     \s+(?:${subElement})
-  //   )*
-  // )
-  // (?=[-_/]|$)
-  // `, 'g');
+  let nameRegex2 = new RegExp(regex`
+    (^|[^${CJK}a-zA-Z${lGuimets}])
+    (
+      (?:${subElement})
+      (?:\s*[-_/]\s*(?:${subElement}))+
+    )
+    (?![${CJK}])
+  `, 'g');
   return str.replace(nameRegex1, (match, p1, p2) => {
     let dividedName = p2.split(/\s+/g);
     for(let name of dividedName) {
       if(KEYWORD_BLACKLIST.includes(name.toLowerCase())) return match;
     }
     return `${p1}《${p2}》`;
-  }).replace(nameRegex2, (match, p1='', p2, p3='') => {
+  }).replace(nameRegex2, (match, p1='', p2) => {
     let dividedName = p2.split(/[-_/]+|\s+/g);
     for(let name of dividedName) {
       if(KEYWORD_BLACKLIST.includes(name.toLowerCase())) return match;
