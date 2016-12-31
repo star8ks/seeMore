42cccf4cbb3a08760fe2b82c0f894ee6c970788a
ray7551 <ray7551@gmail.com>
2017-01-01 00:26:41 +0800

    Icon.search

--- src/common/db/Icon.js
+++ src/common/db/Icon.js
@@ -1,5 +1,23 @@
 import DB from './DB';
 import localforage from '../localforage-bluebird';
 
+/**
+ * @Why Icon is a DB, but not a field of Engine?
+ * Cause technically icon is related to host, and an engine may have many host
+ * @type {DB}
+ */
 let Icon = new DB(localforage, 'icon');
+/**
+ *
+ * @param {[]|String} hosts
+ */
+Icon.search = function (hosts) {
+  hosts = Array.isArray(hosts) ? hosts : [hosts];
+  let searchPromises=[];
+  for (let host of hosts) {
+    searchPromises.push(Icon.get(host));
+  }
+  return Promise.all(searchPromises);
+};
+
 export default Icon;
